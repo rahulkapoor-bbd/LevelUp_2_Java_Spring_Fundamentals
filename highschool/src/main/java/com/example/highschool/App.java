2838e364a9924877cbefaca510e4b11a3790fb2b
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-03-21 15:52:08 +0200

    Added Student and CultiralActivity classes

--- highschool/src/main/java/com/example/highschool/App.java
+++ highschool/src/main/java/com/example/highschool/App.java
@@ -6,7 +6,5 @@ public class App
 {
     public static void main( String[] args )
     {
-        Student examplStudent = new Student("216000924");
-        System.out.println(examplStudent.getStudentNumber());
-    }
+        
 }
