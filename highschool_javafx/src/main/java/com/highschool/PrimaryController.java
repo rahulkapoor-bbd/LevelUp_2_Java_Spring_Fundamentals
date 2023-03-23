9fb0b60425bd9d7d6157ef723f6c323cc745afd8
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-03-23 15:16:08 +0200

    Added db code

--- highschool_javafx/src/main/java/com/highschool/PrimaryController.java
+++ highschool_javafx/src/main/java/com/highschool/PrimaryController.java
@@ -3,7 +3,15 @@ package com.highschool;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class PrimaryController {
+
+    @FXML
+    private Button btnLogout;
 
     @FXML
     private void switchToStudentView() throws IOException {
@@ -13,5 +21,16 @@ public class PrimaryController {
     @FXML
     private void switchToTeacherView() throws IOException {
         App.setRoot("teacher");
+    }
+
+    @FXML
+    private void getData() {
+        DbConnection connection = new DbConnection();
+        connection.connect();
+    }
+
+    @FXML
+    private void initialize() throws IOException {
+        getData();
     }
 }
--- highschool_javafx/src/main/java/module-info.java
+++ highschool_javafx/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.highschool {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
     opens com.highschool to javafx.fxml;
     exports com.highschool;
 }
